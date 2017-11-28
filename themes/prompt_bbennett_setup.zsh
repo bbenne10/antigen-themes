cd0eea5e07405fe9099ddbc8aac8ca0284ce2907
Bryan Bennett <bbenne10@gmail.com>
2017-11-28 10:51:45 -0500

    Add prezto compatible theme

---
+++ themes/prompt_bbennett_setup.zsh
@@ -0,0 +1,36 @@
+prompt_bbennett_pyenv () {
+  local ver=$(pyenv version-name)
+  if [ -n "$ver" -a "$ver" != 'system' ]; then
+    echo "($ver)"
+  fi
+}
+
+prompt_bbennett_precmd () {
+  vcs_info
+
+
+  PROMPT="${vcs_info_msg_0_}%(?.%{$fg[green]%}.%{$fg[red]%})❯ %{$reset_color%}"
+  RPROMPT=$'\$(prompt_bbennett_pyenv) %E%{$fg[magenta]%}%~%{$reset_color%}'
+}
+
+prompt_bbennett_setup () {
+  autoload -Uz vcs_info
+  autoload -Uz add-zsh-hook
+  autoload -Uz colors
+  colors
+  export LSCOLORS="Gxfxcxdxbxegedabagacad"
+
+  setopt prompt_subst
+
+  zstyle ':vcs_info:*' enable git
+  zstyle ':vcs_info:git*' formats "%b%u% "
+  zstyle ':vcs_info:git*' actionformats '%b (%a)'
+  zstyle ':vcs_info:git*' unstagedstr '±'
+
+  zstyle ':vcs_info:git:*' check-for-changes true
+
+  prompt_opts=(cr subst percent)
+  add-zsh-hook precmd prompt_bbennett_precmd
+}
+
+prompt_bbennett_setup "$@"
